fa93be7f73dc0016a6ed3bd10c763904e10df1e8
estefanymaria <estefanymaria0908@gmail.com>
2024-08-12 11:55:34 -0300

    tarefas

---
+++ Algoritmos 24.2/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+Console.WriteLine("Digite seu nome:");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine("Olá, " + nome + ", digite o valor de a");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite agora o valor de b");
+
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("A soma de a e b é: " + (a+b));
+*/
+
+/*
+Console.WriteLine("Digite um número: ");
+
+int numero = int.Parse(Console.ReadLine());
+
+bool par = numero % 2 == 0;
+
+if (par)
+{
+    Console.WriteLine("é par");
+}
+else
+{
+    Console.WriteLine("é ímpar");
+}
+*/
+
+/*
+string senha = "2007";
+
+Console.WriteLine("Digite a senha");
+
+string senha_digitada = Console.ReadLine();
+
+if(senha == senha_digitada)
+{
+    Console.WriteLine("Entrou");
+}
+else
+{
+    Console.WriteLine("Senha incorreta");
+}
+*/
+
+/*
+Console.WriteLine("Digite uma nota");
+
+int nota = int.Parse(Console.ReadLine());
+
+if(nota >= 60)
+{
+    Console.WriteLine("Aprovado");
+}
+else if(nota >= 20)
+{
+    Console.WriteLine("Recuperação");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}
+*/
